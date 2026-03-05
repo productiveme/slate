9492748bc33899ed679c03e0217e7c3e68685f4e
Jaco Swarts <jaco@vatfree.com>
2026-03-05 08:31:25 +0200

    Improve markdown rendering with marked

--- components/MarkdownEditor.vue
+++ components/MarkdownEditor.vue
@@ -65,24 +65,13 @@ import { Icon } from '@iconify/vue';
 import { useStorage } from '../composables/useStorage';
 import FloatingToolbar from './FloatingToolbar.vue';
 import { useEventListener } from '@vueuse/core';
+import { marked } from 'marked';
 
 const { storage } = useStorage();
 
 function markdownToHTML(markdown) {
   if (!markdown) return '';
-  
-  let html = markdown
-    .replace(/^### (.*$)/gim, '<h3>$1</h3>')
-    .replace(/^## (.*$)/gim, '<h2>$1</h2>')
-    .replace(/^# (.*$)/gim, '<h1>$1</h1>')
-    .replace(/\*\*(.*)\*\*/gim, '<strong>$1</strong>')
-    .replace(/\*(.*)\*/gim, '<em>$1</em>')
-    .replace(/!\[(.*?)\]\((.*?)\)/gim, '<img alt="$1" src="$2" />')
-    .replace(/\[(.*?)\]\((.*?)\)/gim, '<a href="$2">$1</a>')
-    .replace(/\n\n/gim, '</p><p>')
-    .replace(/\n/gim, '<br />');
-  
-  return '<p>' + html + '</p>';
+  return marked(markdown);
 }
 
 const props = defineProps({
